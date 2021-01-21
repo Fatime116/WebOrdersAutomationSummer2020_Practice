507da4ad8c9a368a9151cf892c695cb879bad689
Fatime116 <patigul116@gmail.com>
2021-01-21 11:58:22 -0800

    added cucumber runner class

--- src/test/java/com/weborders/runners/CucumberRunner.java
+++ src/test/java/com/weborders/runners/CucumberRunner.java
@@ -1,5 +1,17 @@
 package com.weborders.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)//RunWith from Junit
+@CucumberOptions(// we need to have our settings
+
+        glue="com/weborders/step_definitions",
+        features = "src/test/resources/features",
+        dryRun = true,
+        publish = true
+
+)
 public class CucumberRunner {
 }
